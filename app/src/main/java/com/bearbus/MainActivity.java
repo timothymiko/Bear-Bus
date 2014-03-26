f981e9485b05b248394ac1dfffc9108ee4aa9e60
timothymiko <timothymiko@gmail.com>
2014-03-26 18:39:25 -0400

    removed options menu

--- app/src/main/java/com/bearbus/MainActivity.java
+++ app/src/main/java/com/bearbus/MainActivity.java
@@ -2,8 +2,6 @@ package com.bearbus;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -35,26 +33,5 @@ public class MainActivity extends ActionBarActivity {
         LatLng oaklandUniversity = new LatLng(42.673776, -83.207247);
         CameraPosition newPosition = CameraPosition.fromLatLngZoom(oaklandUniversity, 14.0f);
         map.moveCamera(CameraUpdateFactory.newCameraPosition(newPosition));
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
