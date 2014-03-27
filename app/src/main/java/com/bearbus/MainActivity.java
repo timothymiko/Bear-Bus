e47d5d2e9851e14afbb63b9ff8f1fa235615c0df
evankathawa <thisiznotevan@gmail.com>
2014-03-27 13:00:13 -0400

    Added a location markers for oncampus stops

--- app/src/main/java/com/bearbus/MainActivity.java
+++ app/src/main/java/com/bearbus/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -33,5 +34,48 @@ public class MainActivity extends ActionBarActivity {
         LatLng oaklandUniversity = new LatLng(42.673776, -83.207247);
         CameraPosition newPosition = CameraPosition.fromLatLngZoom(oaklandUniversity, 14.0f);
         map.moveCamera(CameraUpdateFactory.newCameraPosition(newPosition));
+
+        // Markers for Northern On-Campus
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.677324, -83.218395))
+                .title("Human Health Building"));
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.677356, -83.214662))
+                .title("Hamlin Circle"));
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.67891, -83.211062))
+                .title("P11 North Side"));
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.679576, -83.210498))
+                .title("Ann V. Nicholson 4000's"));
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.675285, -83.218084))
+                .title("Oakland Center/Wilson"));
+
+        // Markers for Central On-Campus
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.67639, -83.210573))
+                .title("P11 South Side"));
+
+        // Markers for Southern On-Campus
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.672848, -83.22473))
+                .title("Extended Stay America (University Drive)"));
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.668888, -83.215573))
+                .title("P35/37"));
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.672379,-83.212354))
+                .title("Pawley/Varner"));
+
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(42.674077, -83.213969))
+                .title("O'rena/Recreation Center"));
     }
 }
