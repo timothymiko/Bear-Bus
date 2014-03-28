4bc31e260635b5b2b0b98ded1ea0118abb48fb1e
Phill Szymanski <phillszymanski@gmail.com>
2014-03-27 21:05:26 -0400

    More Stuff from old version.

--- app/src/main/java/com/bearbus/MainActivity.java
+++ app/src/main/java/com/bearbus/MainActivity.java
@@ -8,10 +8,41 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MarkerOptions;import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.PopupWindow;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.util.List;
+import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.content.DialogInterface;
+import com.parse.Parse;
+import com.parse.PushService;
+import android.widget.TextView;
+import com.bearbus.R;
+
+
+
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String PARSE_APP_ID = "Hr5DPwQzhmzzST1sNzME8ssu3zaDxRZgtLO10Zxk";
+    private final String PARSE_CLIENT_KEY = "49AgCaNyWzaFFCgHFPgS3NK0lEjTpLNPDDYBrswX";
+    ArrayList stops= new ArrayList();
+    private Spinner spinner;
+    private Spinner dropSpinner;
+    private Button btnRequest;
 
     private GoogleMap map;
 
@@ -19,6 +50,63 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parse.initialize(this, PARSE_APP_ID, PARSE_APP_ID);
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+        //button
+        this.btnRequest = (Button) this.findViewById(R.id.request);
+        this.btnRequest.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+            }
+        });
+
+        List<String> SpinnerArray = new ArrayList<String>();
+        SpinnerArray.add("Parking Lot 3");
+        SpinnerArray.add("Parking Lot 11");
+        SpinnerArray.add("Parking Lot 35");
+        SpinnerArray.add("Recreation Center");
+        SpinnerArray.add("Meadow Brook Road");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, SpinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = (Spinner) findViewById(R.id.pspinner);
+        spinner.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, SpinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner dspinner = (Spinner) findViewById(R.id.dspinner);
+        dropSpinner.setAdapter(adapter1);
+
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int arg2, long arg3) {
+                // TODO Auto-generated method stub
+                Object item = arg0.getItemAtPosition(arg2);
+                if (item!=null) {
+                    Toast.makeText(MainActivity.this, item.toString(),
+                            Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(MainActivity.this, "Selected",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
 
         // Get a handle to the Map Fragment
         map = ((SupportMapFragment) getSupportFragmentManager()
@@ -77,5 +165,22 @@ public class MainActivity extends ActionBarActivity {
         map.addMarker(new MarkerOptions()
                 .position(new LatLng(42.674077, -83.213969))
                 .title("O'rena/Recreation Center"));
+
+
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }*/
+
+
+
+    public void addListenerOnSpinnerItemSelection() {
+        spinner = (Spinner) findViewById(R.id.pspinner);
+        dropSpinner = (Spinner) findViewById(R.id.dspinner);
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
     }
 }
